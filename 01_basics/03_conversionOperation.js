8f2845ada360bb1c14a05b4955edee8bff8e143a
YUVRAJ SINGH BISHT <126282267+yuvi-01@users.noreply.github.com>
2024-11-12 17:25:11 +0000

    conersion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// *************************  Operations ************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Hey"
+let str2 = "  UV"
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// operation will happen acc to the first datatype 
+// console.log("1" + 2 + 2); // means  here they'll act as String
+                          
+// console.log(1 + 2 + "2"); //here they'll act as Int
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter; // prefix and postfix
+console.log(gameCounter);
+
