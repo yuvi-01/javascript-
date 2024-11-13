fd753c91fd37fa5e77bc0e0d08ed7727ef12600e
YUVRAJ SINGH BISHT <126282267+yuvi-01@users.noreply.github.com>
2024-11-13 07:13:51 +0000

    datatypesSummary

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -39,9 +39,35 @@ console.log(typeof bigNumber);
 console.log(typeof outsideTemp);
 console.log(typeof scoreValue);
 console.log(typeof myFunction);
-console.log(typeof heros);
+
+console.log(typeof heros); //array in js are object
+
 console.log(typeof myObj);
 console.log(typeof anotherId);
 
 
+
+
+//summary of above codes
+// Primitive Types:
+
+// Number (score, scoreValue)
+// Boolean (isLoggedIn)
+// null (outsideTemp)
+// undefined (userEmail)
+// Symbol (id, anotherId)
+// BigInt (bigNumber)
+// Reference Types:
+
+// Array (heros)
+// Object (myObj)
+// Function (myFunction)
+// Output of typeof checks:
+// typeof bigNumber returns "bigint": BigInt represents large integers.
+// typeof outsideTemp returns "object": This is a known quirk in JavaScript where typeof null returns "object", even though null is a primitive type.
+// typeof scoreValue returns "number".
+// typeof myFunction returns "function".
+// typeof heros returns "object": Arrays in JavaScript are objects.
+// typeof myObj returns "object".
+// typeof anotherId returns "symbol".
 
