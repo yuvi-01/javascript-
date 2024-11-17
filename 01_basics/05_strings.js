ddaf91bd42380ac4446fb20509d64935eeecac87
YUVRAJ SINGH BISHT <126282267+yuvi-01@users.noreply.github.com>
2024-11-17 17:05:04 +0000

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,21 +7,22 @@ const repoCount = 50
 
 console.log(`Hello My Name is ${name} and my repoCount is ${repoCount}`);
 
-const gameName = new String('Yuvraj')
+const gameName = new String('Yuvraj') // other way
 
 
 //accesing function in String
 
-console.log(gameName[0]);
+console.log(gameName[0]); // accessing key values 
 console.log(gameName[6]);
 
 console.log(gameName.__proto__);
 console.log(gameName.length);
+
 console.log(gameName.toUpperCase()); // does not change the original value of string //STACK 
 //  STACK=> (Primitive-)[copy of original value is given ] 
 
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('r'));
+console.log(gameName.charAt(2)); //character  T THE position
+console.log(gameName.indexOf('r'));//  finDING pOSITION OF ELEMENTS
 
 const newString =gameName.substring(0,4)
 console.log(newString);
